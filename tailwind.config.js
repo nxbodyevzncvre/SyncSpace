544bb432c1828e6ca6532d95eb2ec5e8741dbd4b
Danil <danil.ignashov@student.ukf.sk>
2024-12-03 11:02:15 +0100

    trying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
+  relative: true,
+
   content: [
     './index.html',
-    './planets/**/*.{html,js}',
-    './css/planets/**/*.css',
+    './planets/**/*.html',
+    './css/**/*.css',
     './js/**/*.js',
-    "./js/*.js"
+    './js/*.js'
   ],
   safelist: [
-    /^text-\[\d+rem\]/,
-    /^w-\[\d+rem\]/,   
-    /^w-\[\d+vw\]/, 
-    /^h-\[\d+rem\]/,
-    /^h-\[\d+vh\]/         
+    /^text-\[\d+(rem|px|%)\]/,
+    /^w-\[\d+(rem|vw|px|%)\]/,
+    /^h-\[\d+(rem|vh|px|%)\]/
   ],
   theme: {
     screens: {
-      "extra": "2560px",
-      "actual": "1920px",
-      'laptop': '1600px',
-      'tablet': '1400px',
-      "middle": '1200px',
-      'tablet2': '1000px',
-      'phones': '800px',
+      extra: '2560px',
+      actual: '1920px',
+      laptop: '1600px',
+      tablet: '1400px',
+      middle: '1200px',
+      tablet2: '1000px',
+      phones: '800px'
     },
     extend: {
       backgroundImage: {
-        "solar-sys": "url('./img/backround/Galaxy Starry Night.jpg')",
-        "slider": "url('./img/backround/image.png')"        
+        'solar-sys': "url('img/backround/Galaxy Starry Night.jpg')",
+        'slider': "url('/img/backround/image.png')"
       },
       fontFamily: {
-        itim: ["Itim", "cursive"],
-        inter: ["Inter", "cursive"],
+        itim: ['Itim', 'cursive'],
+        inter: ['Inter', 'cursive']
       },
       colors: {
-        block: "#333333",
-      },
-    },
+        block: '#333333'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
