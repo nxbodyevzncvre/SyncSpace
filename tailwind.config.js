35a5b982f2b49c423fb1d72100d8cd8b7a4755ac
Danil <danil.ignashov@student.ukf.sk>
2024-12-04 18:06:29 +0100

    trying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,50 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  relative: true,
 
-  content: {
-    relative:true,
-    files:[
-    './index.html',
-    './planets/**/*.html',
-    './css/**/*.css',
-    './js/**/*.js',
-    './js/*.js'
-    ],
-  },
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html'
+  ],
+  // content: ["index.html", 
+  //   "/planets/mercury/mercury.html",
+  //   "/planets/venus/venus.html", 
+  //   "/planets/earth/earth.html", 
+  //   "/planets/mars/mars.html", 
+  //   "/planets/jupiter/jupiter.html",
+  //   "/planets/saturn/saturn.html",
+  //   "/planets/uranus/uranus.html", 
+  //   "/planets/neptun/neptun.html",],
   theme: {
-    screens: {
-      extra: '2560px',
-      actual: '1920px',
-      laptop: '1600px',
-      tablet: '1400px',
-      middle: '1200px',
-      tablet2: '1000px',
-      phones: '800px'
+    screens:{
+      "extra": "2560px",
+      "actual": "1920px",
+      'laptop': '1600px',
+      'tablet': '1400px',
+      "middle": '1200px',
+      'tablet2': '1000px',
+      'phones': '800px',
     },
     extend: {
-      backgroundImage: {
-        'solar-sys': "url('/img/backround/Galaxy Starry Night.jpg')",
-        'slider': "url('/img/backround/image.png')"
+      backgroundImage:{
+        "solar-sys": "url('/img/backround/Galaxy Starry Night.jpg')",
+        "slider": "url('/img/backround/image.png')",
       },
-      fontFamily: {
-        itim: ['Itim', 'cursive'],
-        inter: ['Inter', 'cursive']
+      fontFamily:{
+        itim: ["Itim", "cursive"],
+        inter: ["Inter", "cursive"],
       },
-      colors: {
-        block: '#333333'
-      }
-    }
+      colors:{
+        block: "#333333",
+      },
+      fontSize:{
+        xl: "10rem",
+      },
+
+
+    },
   },
-  plugins: []
+  plugins: [],
 }
+
